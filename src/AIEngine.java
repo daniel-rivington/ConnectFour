07ef1a3faedbcdef8038e1e25b5052f3531f0e32
D.V.R <daniel.vlad.rivington@gmail.com>
2024-12-27 19:03:04 -0500

    Added Colour

--- src/AIEngine.java
+++ src/AIEngine.java
@@ -26,6 +26,10 @@ public class AIEngine {
                 // test on a clone if it wins
                 GameEngine geClone = (GameEngine) simulatedGame.clone();
                 if ( geClone.anyWins(geClone.dropPiece(i, player),player)) return i;
+                // see if you can block a move by the opposing guy:
+                geClone = (GameEngine) simulatedGame.clone(); // first create a new clone so it doesn't have value from above.
+                if (geClone.anyWins(geClone.dropPiece(i,(player==1)?2:1),(player==1)?2:1)) return i;
+
              if (simulatedGame.boardState[0][i] == null) available.add(new Integer(i));
          }
         } catch (CloneNotSupportedException ce )
